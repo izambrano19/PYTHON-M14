54793511677d7d39213ef73198da6420438a5e31
super <izambrano19@ilg.cat>
2022-04-22 17:05:52 +0200

    PYTHON-3COMMIT - 22/04/2022

--- alineaments.py
+++ alineaments.py
@@ -27,13 +27,26 @@ for i in SeqIO.parse("ls_orchid.fasta","fasta"):
 #print(diccionari["hola"])
 #print(diccionari["Z78533.1"])
 
+
+
 def alinea(cadena1,cadena2):
+    valor = 0
+    for i in range(len(cadena1)):
+        if cadena1[i] == cadena2[i]:
+            valor += 1
+        else:
+            valor -= 1
+    return valor
 
-    return
+#claus = diccionari.keys() Esto no va ya que tiene que hacer una lista de los valores
+claus = list(diccionari.keys())
 
-claus = diccionari.keys()
 
-for i in range(len(claus)):#Desde i hasta la longitud del vector de claus
-    for j in range(i+1,len(claus)):#Hacemos un segundo bucle que comienza uno más a la derecha, que va contando de 1 en 1 según la longitud de i.
-        alinea(claus[i],claus[j])
+with open("informe.txt","w") as simple_handle:
+    for i in range(len(claus)):#Desde i hasta la longitud del vector de claus
+        for j in range(i+1,len(claus)):#Hacemos un segundo bucle que comienza uno más a la derecha, que va contando de 1 en 1 según la longitud de i.
+            #primer = diccionari[claus[i]]
+            #segon = diccionari[claus[j]]
+            alineament = alinea(diccionari[claus[i]],diccionari[claus[j]])
+            simple_handle.write("L'ALINEAMENT ES " + str(alineament) + ":\n")
 
